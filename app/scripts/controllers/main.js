1243be4b4a913a50884d458fce9c92d0f9bff77b
Mark Gable <mgable43@gmail.com>
2015-11-02 18:55:49 -0800

    added error handling for no search results

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,13 @@
  */
 angular.module('leftfieldlabsApp')
   .controller('MainCtrl', function ($scope, GetDistance, CONFIG, Origin, Places) {
+  	var origin_address_reset;
 
   	$scope.origin = Origin;
   	$scope.places = Places;
-  	$scope.place = Places[0]
+  	$scope.place = Places[0];
+
+  	remember($scope.origin.formatted_address);
 
 	$scope.updateMap = function(location2){
 		$scope.place = location2;
@@ -24,11 +27,26 @@ angular.module('leftfieldlabsApp')
 
 	$scope.setOrigin = function(which){
 		GetDistance.getOrigin(which).then(function(data){
-			$scope.origin = data;
-			GetDistance.getPlaces(CONFIG.places).then(function(data){
-				$scope.places = data;
-				$scope.place = $scope.places[0];
-			});
+			if (data){
+				$scope.origin = data;
+				remember($scope.origin.formatted_address);
+				GetDistance.getPlaces(CONFIG.places).then(function(data){
+					$scope.places = data;
+					$scope.place = $scope.places[0];
+				});
+			} else {
+				resetOrigin(origin_address_reset);
+				alert ("No results where found for that search");
+			}
 		});
 	}
+
+	function remember(which){
+		origin_address_reset = which; 
+	}
+
+	function resetOrigin(which){
+		$scope.origin.formatted_address = which;
+	}
+
   });
